2ddf01ede2647481953f92bc4e6b4a6dbd1c26b6
NANDHAGOPAN.G <130740370+Nandhagopan10@users.noreply.github.com>
2023-06-07 22:28:48 +0530

    Add files via upload

---
+++ arithmetic expression/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+
+void main()
+{
+    float a,b,c,d,e,f,g,x;
+    printf("Enter the values of a,b,c,d,e,f,g: \n");
+    scanf("%f %f %f %f %f %f %f",&a,&b,&c,&d,&e,&f,&g);
+    x=((a-b/c*d+e)*(f+g));
+    printf("The solution of  the expression is %f",x);
+}
