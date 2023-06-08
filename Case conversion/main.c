20c67e2bde834e622864450c8ffe38b313f68e85
NANDHAGOPAN.G <130740370+Nandhagopan10@users.noreply.github.com>
2023-06-08 22:16:36 +0530

    Add files via upload

---
+++ Case conversion/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+
+    int diff=32;
+    char letter;
+    printf("Enter the character: \n");
+    scanf("%c",&letter);
+    if((letter>=65)&&(letter<=97))
+    {
+        printf("%c is in upper case \n",letter);
+        printf("The lower case is %c \n",letter+32);
+    }
+    else if((letter>=97)&&(letter<=122))
+    {
+        printf("%c is in lower case \n",letter);
+        printf("The upper case is %c \n",letter-32);
+    }
+    else
+    {
+        printf("%c is an invalid character \n",letter);
+    }
+}
