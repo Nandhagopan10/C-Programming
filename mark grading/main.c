658164711f08c984a49fc3cfaa8fbd7a398ae199
NANDHAGOPAN.G <130740370+Nandhagopan10@users.noreply.github.com>
2023-06-10 23:12:54 +0530

    Add files via upload

---
+++ mark grading/main.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+void main()
+{
+    int mark;
+    printf("Enter your marks: \n");
+    scanf("%d",&mark);
+    if(mark>=90)
+    {
+        printf("The grade is S \n");
+    }
+    else if(mark>=80)
+    {
+        printf("The grade is A \n");
+    }
+    else if(mark>=75)
+    {
+        printf("The grade is B+ \n");
+    }
+    else if(mark>=70)
+    {
+        printf("The grade is B \n");
+    }
+    else if(mark>=65)
+    {
+        printf("The grade is C+ \n");
+    }
+    else if(mark>=60)
+    {
+        printf("The grade is C \n");
+    }
+    else
+    {
+        printf("The student is just passed \n");
+    }
+}
+
