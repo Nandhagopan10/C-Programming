66ecf64ed0015dc7344b651c46fccc53d056ebb8
NANDHAGOPAN.G <130740370+Nandhagopan10@users.noreply.github.com>
2023-06-10 23:14:05 +0530

    Add files via upload

---
+++ roots of quadratic eqn/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+
+void main()
+{
+    float a,b,c,n,root1,root2;
+    printf("Enter the values of a,b,c: \n");
+    scanf("%f %f %f",&a,&b,&c);
+    n=(b*b)-(4*a*c);
+    root1=(-b+n)/(2*a);
+    root2=(-b-n)/(2*a);
+    printf("The roots are %f,%f",root1,root2);
+}
